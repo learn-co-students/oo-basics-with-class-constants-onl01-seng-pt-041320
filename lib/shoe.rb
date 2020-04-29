3b64cbd6513038d6fb7463eacfb19b2c573f8a18
Jared  E <jared.epps322@gmail.com>
2020-04-29 18:40:15 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,51 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  
+  BRANDS = []
+  
+  attr_accessor :color, :size, :material, :condtion
+  attr_reader :brand 
+  
 
   def initialize(brand)
-    @brand = brand
+   @brand = brand
+  # if !BRANDS.include?(brand)
+  #   BRANDS << brand
+  # end 
+  # BRANDS << brand unless BRANDS.include?(brand)
+BRANDS << brand if !BRANDS.include?(brand)
+  
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand=(brand)
+    @brand = brand 
+    BRANDS << brand 
+  end 
 
 end
+
+
+# class Book
+#   attr_accessor :author, :page_count, 
+#   attr_reader :title, :genre
+
+#   GENRES = []
+  
+#   def initialize(title)
+#     @title = title
+#   end
+
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+  
+  
+#   def genre=(genre)
+#     @genre = genre 
+#     GENRES << genre 
+#   end 
+#   end
