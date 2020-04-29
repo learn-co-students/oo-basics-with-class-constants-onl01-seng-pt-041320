08453dd7b5e3444388e19597b34889114f93ce62
adeeba reaz <adeebarivera@outlook.com>
2020-04-29 22:37:04 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,36 @@
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
+
+#   def initialize(title)
+#     @title = title
+#   end
+
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+
+# end
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
-  def initialize(title)
-    @title = title
+  def initialize(title)	  
+    @title = title	    
+  end	  
+
+  GENRES = []
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre unless GENRES.any? { |g| g == genre }
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
 
+  def turn_page	  
+    puts "Flipping the page...wow, you read fast!"	
+  end
+  
 end
+
